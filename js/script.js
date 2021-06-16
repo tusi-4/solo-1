d87f7d43449890ef05c1294fbbb06bfdf3e3a868
Marta Wojdat <marjow4@gmail.com>
2021-06-16 14:08:25 +0200

    Add basics

---
+++ js/script.js
@@ -0,0 +1,12 @@
+{
+	'use strict';
+
+	function toggleMenu(visible) {
+	  document.querySelector('.menu').classList.toggle('show', visible)
+	}
+
+	document.querySelector('.hamburger').addEventListener('click', function(e) {
+	  e.preventDefault();
+	  toggleMenu()
+	});
+}
