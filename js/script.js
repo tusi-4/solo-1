0518360daac64c99f32301aaa3bc52f48417dba7
Marta Wojdat <marjow4@gmail.com>
2021-06-16 21:57:24 +0200

    Add sidebar section with styles and media queries

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 {
 	'use strict';
 
-	function toggleMenu(visible) {
-	  document.querySelector('.menu').classList.toggle('show', visible)
+	function toggleMenu() {
+	  const sidebar = document.querySelector('.section--sidebar');
+	  const elemsToHide = document.querySelectorAll('.to-hide');
+	  const avatar = document.querySelectorAll('.avatar');
+
+	  sidebar.classList.toggle('wide');
+
+	  for(let elem of elemsToHide){
+	  	elem.classList.toggle('hidden');
+	  };
+
+	  avatar.classList.toggle('mini');
 	}
 
-	document.querySelector('.hamburger').addEventListener('click', function(e) {
-	  e.preventDefault();
-	  toggleMenu()
+	document.querySelector('.hamburger').addEventListener('click', function(event) {
+	  event.preventDefault();
+	  toggleMenu();
 	});
 }
