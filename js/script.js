7a5c61ac54b0b68a2152cd67a87fec92449eb3db
Marta Wojdat <marjow4@gmail.com>
2021-06-17 20:31:52 +0200

    Add main section and statistic page with media query and styles

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@
 	function toggleMenu() {
 	  const sidebar = document.querySelector('.section--sidebar');
 	  const elemsToHide = document.querySelectorAll('.to-hide');
-	  const avatar = document.querySelectorAll('.avatar');
+	  const avatar = document.querySelector('.avatar');
+	  const main = document.querySelector('.section--main');
 
 	  sidebar.classList.toggle('wide');
 
@@ -13,6 +14,8 @@
 	  };
 
 	  avatar.classList.toggle('mini');
+
+	  main.classList.toggle('jump');
 	}
 
 	document.querySelector('.hamburger').addEventListener('click', function(event) {
